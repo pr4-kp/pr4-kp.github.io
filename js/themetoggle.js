e49b92c9c9cf90513b5609d162345ae6855bf64c
PramanaSaldin <PramanaSaldin@users.noreply.github.com>
2022-12-20 04:11:08 +0000

    deploy: 0b7fd6df7ee25761dcd0038e8f702fee242651f4

--- js/themetoggle.js
+++ js/themetoggle.js
@@ -2,11 +2,17 @@ function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
     if (mode === "dark") {
         document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
+        document.getElementById("dark-mode-toggle").innerHTML = `
+        <script src=\"/js/feather.min.js\"></script>
+        <i data-feather=\"sun\"></i>
+        `;
         feather.replace()
     } else if (mode === "light") {
         document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
+        document.getElementById("dark-mode-toggle").innerHTML = `
+        <script src=\"/js/feather.min.js\"></script>
+        <i data-feather=\"moon\"></i>
+        `;
         feather.replace()
     }
 }
