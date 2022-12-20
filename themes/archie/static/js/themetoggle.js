644e407a5e51856bdcce9e74bb990a9b0a54ac92
PramanaSaldin <saldin@wisc.edu>
2022-12-20 12:22:18 -0600

    another attempt to fix light/dark

--- themes/archie/static/js/themetoggle.js
+++ themes/archie/static/js/themetoggle.js
@@ -2,11 +2,11 @@ function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
     if (mode === "dark") {
         document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<a>light mode</a>";
+        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
         feather.replace()
     } else if (mode === "light") {
         document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<a>dark mode</a>";
+        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
         feather.replace()
     }
 }
